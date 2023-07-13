365579ed5de02e78097e4df9fb445d188704a226
Rawan Atef <rawanatef041@gmail.com>
2023-07-13 16:41:27 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
-/** 
- *main - Entry point
- *
- * Description: A C program that prints with put function 
- * 
- * Return: Always 0 (Success)
+/**
+ * main - main block
+ * Return: 0
  */
-int main(void){
-         puts("\"Programming is like building a multilingual puzzle");
-         return(0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
-/** 
- *main - Entry point
- *
- * Description: A C program that prints with printf function 
- * 
- * Return: Always 0 (Success)
+/**
+ * main - main block
+ * Return: 0
  */
-int main(void){
-         puts("with proper grammar, but the outcome is a piece of art,\n");
-         return(0);
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
